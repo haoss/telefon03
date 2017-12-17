f69fbb1ceef37ee8cf0523c3d13bc5a659b716d6
haoss <haoss_ut@mail.ru>
2017-12-17 22:43:29 +0200

    no message

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -57,12 +57,32 @@ $(document).on('ready', function(){
   });
 
   $('.open-popup-link').magnificPopup({
-    type: 'inline',
-    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    type: 'inline', // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    showCloseBtn: false
+  });
+
+  $('.popup__close').on('click', function(){
+    $.magnificPopup.close();
   });
 
   mobileNav();
-  navRemove()
+  navRemove();
+
+  $('.product__content').on('click', function() {
+    $(this).addClass('is-active');
+    $(this).css({height: '100%', overflow: 'visible'});
+    $('.product__content-wrapper div').css({float: 'none', margin: '0'});
+  });
+
+  $('.about__content').on('click', function() {
+    $(this).addClass('is-active');
+    $(this).css({height: '100%', overflow: 'visible'});
+    $('.about__content-wrapper div').css({float: 'none', margin: '0'});
+  });
+
+  priceTable();
+  priceTableCarousel();
+  faq();
 
   // Chrome Smooth Scroll
   try {
@@ -86,6 +106,7 @@ $(window).on('load', function() {
 // $(window).on('scroll', function() { console.log('scroll'); });
 $(window).on('resize', function() {
   navRemove();
+  // priceTableCarousel();
 });
 
 /*
@@ -179,4 +200,139 @@ function navRemove(){
     nav.removeClass('is-active');
     shadow.removeClass('is-active');
   }
+}
+
+function priceTable(){
+  var tableBlock = $('.price__table-block');
+
+  tableBlock.each(function(){
+    var _this = $(this);
+    var blockWrapper = _this.find('.price__table-block__wrapper');
+    var row = blockWrapper.find('.row');
+
+    if (row.length > 5) {
+      blockWrapper.parent().append('<div class="price__table-see-all"><a href="#!" class="btn btn--red btn--small btn--border btn--wauto">смотреть все цены</a></div>');
+      row.addClass('hidden');
+      row.slice(0,5).removeClass('hidden');
+    }
+
+    $(document).on('click', '.price__table-see-all', function(){
+      if (_this.hasClass('active')) {
+        row.removeClass('hidden');
+        this.remove();
+      }
+    });
+
+  })
+
+}
+
+function priceTableCarousel(){
+  var width = $(window).width();
+  var carousel = $('.price__table-carousel ul');
+  var li = carousel.find('li');
+  var liLength = li.length;
+
+  if (liLength > 3 && width < 480) {
+    li.removeClass('active');
+    carousel.parent().addClass('price__table-carousel--w320');
+    carousel.parents('.price__table-head').addClass('is-carousel');
+    $('.price__table-carousel ul').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true,
+      infinite: true,
+      arrows: true,
+      variableWidth: true,
+      centerPadding: '0px'
+    });
+  }
+
+  if (liLength > 5 && width > 480 && width <= 767) {
+    li.removeClass('active');
+    carousel.parent().addClass('price__table-carousel--w480');
+    carousel.parents('.price__table-head').addClass('is-carousel');
+    $('.price__table-carousel ul').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true,
+      infinite: true,
+      arrows: true,
+      variableWidth: true,
+      centerPadding: '0px'
+    });
+  }
+
+  if (liLength > 8 && width > 767 && width <= 991) {
+    li.removeClass('active');
+    carousel.parent().addClass('price__table-carousel--w767');
+    carousel.parents('.price__table-head').addClass('is-carousel');
+    $('.price__table-carousel ul').slick({
+      slidesToShow: 8,
+      slidesToScroll: 1,
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true,
+      infinite: true,
+      arrows: true,
+      variableWidth: true,
+      centerPadding: '0px'
+    });
+  }
+
+  if (liLength > 10 && width > 991) {
+    li.removeClass('active');
+    carousel.parent().addClass('price__table-carousel--w991');
+    carousel.parents('.price__table-head').addClass('is-carousel');
+    $('.price__table-carousel ul').slick({
+      slidesToShow: 10,
+      slidesToScroll: 1,
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true,
+      infinite: true,
+      arrows: true,
+      variableWidth: true,
+      centerPadding: '0px'
+    });
+  }
+
+  $('.price__table-carousel ul .slick-slide').on('click', function() {
+    $('.price__table-carousel .slick-slide').removeClass('active');
+  });
+
+  $('.price__table-carousel ul').on('beforeChange', function(event,slick,slide,nextSlide) {
+    $('.price__table-carousel').find('.slick-slide').eq(nextSlide).find('a').trigger( 'click' );
+  });
+
+}
+
+function faq(){
+  var faqBlock = '.faq__block',
+      faqBlockAnswer = $('.faq__block-answer'),
+      faqBlockTitle = $('.faq__block-title');
+
+  $(faqBlock).each(function(){
+    var _this = $(this);
+    if (_this.hasClass('is-active')) {
+      _this.find('.faq__block-answer').show();
+    }
+  });
+
+  faqBlockTitle.on('click', function(e){
+    e.preventDefault();
+    var _this = $(this);
+
+    if (!_this.parents(faqBlock).hasClass('is-active')) {
+      _this.parents(faqBlock).addClass('is-active');
+      _this.next(faqBlockAnswer).show();
+    } else {
+      _this.parents(faqBlock).removeClass('is-active');
+      _this.next(faqBlockAnswer).hide();
+    }
+  });
 }
